090de3e62c866fe1f5548212a29db61cad7baedb
Ali Bahadoran <alibahadoran.eng@gmail.com>
2022-01-24 16:03:55 +0330

    check contrast

--- app.js
+++ app.js
@@ -20,6 +20,18 @@ function randomColor() {
         //Add the color to the bg
         div.style.backgroundColor = randomColor;
         hexText.innerText = randomColor;
+        //Check for contrast
+        checkTextContrast(randomColor, hexText);
     });
 }
+
+function checkTextContrast(color, text) {
+    const luminance = chroma(color).luminance();
+    if (luminance > 0.5) {
+        text.style.color = "black";
+    } else {
+        text.style.color = "white";
+    }
+}
+
 randomColor();
